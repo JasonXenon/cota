008024e03e7248e0f8e05554d2defdfde94650e3
JasonXenon <jason.levecq@ifosup.wavre.be>
2023-04-25 23:07:33 +0200

    fix

--- model/connection.php
+++ model/connection.php
@@ -1,11 +1,6 @@
 <?php
     //PAGE DE CONNECTION A LA DB
 
-    // $username = "jasonlve";
-    // $password = "Difbec141021";
-    // $host = "jasonlve.mysql.db";
-    // $dbname = "jasonlve";
-
     $username = "root";
     $password = "";
     $host = "localhost";
