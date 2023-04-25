68ad542c80e7551015cd8ae327ea19d74591d45e
JasonXenon <jason.levecq@ifosup.wavre.be>
2023-04-25 19:49:57 +0200

    update rdv

--- model/delete.php
+++ model/delete.php
@@ -2,17 +2,25 @@
 
 function deleteUser($pk){
     include('connection.php');
-     $query = "DELETE FROM ifo_user
-               WHERE userID = :pk";
-     $query_params = array(":pk" => $pk);
-     try{
-         $stmt = $db->prepare($query);
-         $result = $stmt->execute($query_params);
-     }
-     catch(PDOException $ex){
-         die("Failed query : " . $ex->getMessage());
-     }
-  }
+    try{
+        // Suppression des rendez-vous associés à l'utilisateur
+        $query_rdv = "DELETE FROM ifo_rdv WHERE rdv_pseudo IN (SELECT userPseudo FROM ifo_user WHERE userID = :pk)";
+        $query_params_rdv = array(":pk" => $pk);
+        $stmt_rdv = $db->prepare($query_rdv);
+        $stmt_rdv->execute($query_params_rdv);
+        
+        // Suppression de l'utilisateur
+        $query = "DELETE FROM ifo_user
+                  WHERE userID = :pk";
+        $query_params = array(":pk" => $pk);
+        $stmt = $db->prepare($query);
+        $result = $stmt->execute($query_params);
+    }
+    catch(PDOException $ex){
+        die("Failed query : " . $ex->getMessage());
+    }
+}
+
 
 
 ?>
